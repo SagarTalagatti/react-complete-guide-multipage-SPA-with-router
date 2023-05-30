80e0a5ddbbf9af4ab281d736993ede5b92de05ea
SagarTalagatti <sagartalagatti@gmail.com>
2023-05-30 10:13:54 +0530

    Added comments to Root.js and MainNavigation.js to explain purpose and working of Outlet component and className, end props of NavLink component respectively

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -8,6 +8,7 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
+            {/* className prop of NavLink receives a function which should return the CSS class name that should be added to the underlying anchor tag. This function when called receives an object as an argument and this object has a property isActive (which we can extract using object destructuring as in below example), which is true if the link is currently active, i.e., it led to the currently active route  */}
             {/* the end prop of NavLink is used to tell React that the link should be considered to be active only if the currently active route ends with the specified path. This prop can have value as true or false, and the default value it takes is true as in below example. */}
             <NavLink
               to="/"
